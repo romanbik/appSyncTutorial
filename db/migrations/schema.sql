e3769d731fe26f8338551adefa4c84d4942ddd6e
Roman Bikchentaev <roman.bikchentaev@celadonsoft.com>
2022-10-12 14:41:13 +0300

    db code removed

--- db/migrations/schema.sql
+++ db/migrations/schema.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS project (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    
   PRIMARY KEY (id)
-) engine InnoDb;
+) ENGINE=INNODB;
 
 --- Creation of task table
 CREATE TABLE IF NOT EXISTS task (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   title varchar(200) NOT NULL,
   description varchar(200),
   projectId INT NOT NULL,
@@ -20,11 +20,11 @@ CREATE TABLE IF NOT EXISTS task (
   FOREIGN KEY (projectId)
       REFERENCES project (id)
       ON UPDATE RESTRICT ON DELETE CASCADE
-);
+) ENGINE=INNODB;
 
 --- Creation of sub_task table
 CREATE TABLE IF NOT EXISTS sub_task (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   title varchar(200) NOT NULL,
   description varchar(200),
   taskId INT NOT NULL,
@@ -34,4 +34,4 @@ CREATE TABLE IF NOT EXISTS sub_task (
   FOREIGN KEY (taskId)
       REFERENCES task (id)
       ON UPDATE RESTRICT ON DELETE CASCADE
-);
+) ENGINE=INNODB;
